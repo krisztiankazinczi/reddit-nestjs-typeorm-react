f1aaabb8a0f0dddc2494f2fa25b17275a721cc9c
Krisztian <krisztian.kazinczi@gmail.com>
2021-02-15 22:37:43 +0100

    404 page updated a bit

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,7 +2,10 @@ import Link from "next/link";
 // nextjs will handle this as default 404 page, but subpages are not involved, just the main pages level
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center justify-center bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500" style={{height: '95vh'}}>
+      <h1 className="font-black text-gray-800 text-9xl">
+        404
+      </h1>
       <h1 className="mt-10 mb-4 text-5xl text-gray-800">
         Page not found
       </h1>
